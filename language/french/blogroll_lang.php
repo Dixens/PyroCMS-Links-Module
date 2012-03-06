cc3e4ff41b5f2b2622dd664b5ab6043bd7146e73
Dixens <nicolasdupuy@dixens.net>
2012-03-06 09:22:49 +0100

    Issue with encoding foreign characters.

--- language/french/blogroll_lang.php
+++ language/french/blogroll_lang.php
@@ -1,7 +1,7 @@
 <?php
 $lang['links_admin.title'] = "Liens";
 $lang['links_admin.welcome'] = "Bienvenue dans votre gestionnaire de liens.";
-$lang['links_admin.create_title'] = "Créer un nouveau lien";
+$lang['links_admin.create_title'] = "Cr&eacute;er un nouveau lien";
 $lang['links_admin.edit_title'] = "Editer le lien";
 $lang['links_admin.url_label'] = "URL";
 $lang['links_admin.description_label'] = "Description";
@@ -10,21 +10,21 @@ $lang['links_admin.name_label'] = 'Nom du lien';
 $lang['links_admin.create_button'] = "Ajouter un nouveau lien";
 $lang['links_admin.groups'] = "Groupes de liens";
 $lang['links_admin.group_label'] = "Groupe de liens";
-$lang['links_admin.edit_success'] = "Le lien \"%s\" a été mis à jour";
+$lang['links_admin.edit_success'] = "Le lien \"%s\" a &eacute;t&eacute; mis à jour";
 $lang['links_admin.delete_fail'] = "Le lien est introuvable.";
-$lang['links_admin.delete_success'] = "Le lien a bien été supprimé";
-$lang['links_admin.no_links'] = 'Il n\'y a aucun lien à afficher';
+$lang['links_admin.delete_success'] = "Le lien a bien &eacute;t&eacute; supprim&eacute;";
+$lang['links_admin.no_links'] = 'Il n\'y a aucun lien &agrave; afficher';
 
 $lang['links_groups_admin.title'] = "Groupes de liens";
 $lang['links_groups_admin.welcome'] = "C'est ici que vous gérez vos groupes de liens";
-$lang['links_groups_admin.create_title'] = "Créer un nouveau groupe de liens";
+$lang['links_groups_admin.create_title'] = "Cr&eacute;er un nouveau groupe de liens";
 $lang['links_groups_admin.edit_title'] = "Editer le groupe de liens";
 $lang['links_groups_admin.name_label'] = 'Nom du groupe';
 $lang['links_groups_admin.slug_label'] = 'Slug';
 $lang['links_groups_admin.create_button'] = "Ajouter un groupe de liens";
 $lang['links_groups_admin.description_label'] = 'Description';
-$lang['links_groups_admin.update_success'] = "Groupe mis à jour";
-$lang['links_groups_admin.update_failure'] = "Une erreur s'est produite et le système n'a pas pu sauvegarder le groupe de liens";
-$lang['links_groups_admin.delete_sucess'] = "Le groupe de liens a bien été supprimé";
-$lang['links_groups_admin.no_groups'] = 'Il n\'y a aucun groupe à afficher';
+$lang['links_groups_admin.update_success'] = "Groupe mis &agrave; jour";
+$lang['links_groups_admin.update_failure'] = "Une erreur s'est produite et le syst&agrave;me n'a pas pu sauvegarder le groupe de liens";
+$lang['links_groups_admin.delete_sucess'] = "Le groupe de liens a bien &eacute;t&eacute; supprim&eacute;";
+$lang['links_groups_admin.no_groups'] = 'Il n\'y a aucun groupe &agrave; afficher';
 ?>
