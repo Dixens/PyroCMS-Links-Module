6fb8f59a833d75661c779295564f454546508f22
Nicolas Dupuy <nicolasdupuy@ordinateur-de-nicolas-dupuy.local>
2012-03-06 09:07:02 +0100

    French translation

---
+++ language/french/blogroll_lang.php
@@ -0,0 +1,30 @@
+<?php
+$lang['links_admin.title'] = "Liens";
+$lang['links_admin.welcome'] = "Bienvenue dans votre gestionnaire de liens.";
+$lang['links_admin.create_title'] = "CrŽer un nouveau lien";
+$lang['links_admin.edit_title'] = "Editer le lien";
+$lang['links_admin.url_label'] = "URL";
+$lang['links_admin.description_label'] = "Description";
+$lang['links_admin.target_label'] = "Cible";
+$lang['links_admin.name_label'] = 'Nom du lien';
+$lang['links_admin.create_button'] = "Ajouter un nouveau lien";
+$lang['links_admin.groups'] = "Groupes de liens";
+$lang['links_admin.group_label'] = "Groupe de liens";
+$lang['links_admin.edit_success'] = "Le lien \"%s\" a ŽtŽ mis ˆ jour";
+$lang['links_admin.delete_fail'] = "Le lien est introuvable.";
+$lang['links_admin.delete_success'] = "Le lien a bien ŽtŽ supprimŽ";
+$lang['links_admin.no_links'] = 'Il n'y a aucun lien ˆ afficher';
+
+$lang['links_groups_admin.title'] = "Groupes de liens";
+$lang['links_groups_admin.welcome'] = "C'est ici que vous gŽrez vos groupes de liens";
+$lang['links_groups_admin.create_title'] = "CrŽer un nouveau groupe de liens";
+$lang['links_groups_admin.edit_title'] = "Editer le groupe de liens";
+$lang['links_groups_admin.name_label'] = 'Nom du groupe';
+$lang['links_groups_admin.slug_label'] = 'Slug';
+$lang['links_groups_admin.create_button'] = "Ajouter un groupe de liens";
+$lang['links_groups_admin.description_label'] = 'Description';
+$lang['links_groups_admin.update_success'] = "Groupe mis ˆ jour";
+$lang['links_groups_admin.update_failure'] = "Une erreur s'est produite et le systme n'a pas pu sauvegarder le groupe de liens";
+$lang['links_groups_admin.delete_sucess'] = "Le groupe de liens a bien ŽtŽ supprimŽ";
+$lang['links_groups_admin.no_groups'] = 'Il n'y a aucun groupe ˆ afficher';
+?>
